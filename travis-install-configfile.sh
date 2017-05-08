ab1ee3a803d95f45fc8b0a35eb0a7a0f3906da3a
Eric Mann <eric@eamann.com>
2017-05-08 11:20:57 -0700

    Add Travis Build Status (#2)
    * Set up the config file
    
    * Build Libsodium directly
    
    * Update build status

---
+++ travis-install-configfile.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# The purpose of this file is to install a default
+# e3db profile configuration so tests can execute
+# against a live server.
+
+set -e
+
+# Check if the config is already set
+if [ ! -d "$HOME/.tozny/integration-test" ]; then
+    mkdir -p "$HOME/.tozny/integration-test"
+fi
+
+cat > "$HOME/.tozny/integration-test/e3db.json" <<EOT
+{
+    "version":1,
+    "api_url":"${API_URL}",
+    "api_key_id":"${API_KEY_ID}",
+    "api_secret":"${API_SECRET}",
+    "client_id":"${CLIENT_ID}",
+    "client_email":"${CLIENT_EMAIL}",
+    "public_key":"${PUBLIC_KEY}",
+    "private_key":"${PRIVATE_KEY}"
+}
+EOT
