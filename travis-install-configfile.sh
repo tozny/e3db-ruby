45a0a39f46d2eb2e665aa6589849d3e62dc47552
Eric Mann <eric@tozny.com>
2017-08-09 09:33:34 -0700

    Update Travis config for share client

--- travis-install-configfile.sh
+++ travis-install-configfile.sh
@@ -22,3 +22,21 @@ cat > "$HOME/.tozny/integration-test/e3db.json" <<EOT
     "private_key":"${PRIVATE_KEY}"
 }
 EOT
+
+# Check if the config is already set
+if [ ! -d "$HOME/.tozny/integration-test-share" ]; then
+    mkdir -p "$HOME/.tozny/integration-test-share"
+fi
+
+cat > "$HOME/.tozny/integration-test-share/e3db.json" <<EOT
+{
+    "version":1,
+    "api_url":"${API_URL_2}",
+    "api_key_id":"${API_KEY_ID_2}",
+    "api_secret":"${API_SECRET_2}",
+    "client_id":"${CLIENT_ID_2}",
+    "client_email":"${CLIENT_EMAIL_2}",
+    "public_key":"${PUBLIC_KEY_2}",
+    "private_key":"${PRIVATE_KEY_2}"
+}
+EOT
